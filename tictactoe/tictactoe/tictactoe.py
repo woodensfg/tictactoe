2d93ba9d782b265fd1f6911d6d903f3617fcfccb
Woodens Fanfan <66738140+woodensfg@users.noreply.github.com>
2023-12-21 09:31:30 -0500

    Add files via upload

---
+++ tictactoe/tictactoe/tictactoe.py
@@ -0,0 +1,150 @@
+"""
+Tic Tac Toe Player
+"""
+
+import copy
+import math
+
+X = "X"
+O = "O"
+EMPTY = None
+
+
+def initial_state():
+    """
+    Returns starting state of the board.
+    """
+    return [[EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]]
+
+
+def player(board):
+    """
+    Returns player who has the next turn on a board.
+    """
+    x_count = sum(row.count(X) for row in board)
+    o_count = sum(row.count(O) for row in board)
+
+    return X if x_count == o_count else O
+
+
+def actions(board):
+    """
+    Returns set of all possible actions (i, j) available on the board.
+    """
+    possible_actions = set()
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == EMPTY:
+                possible_actions.add((i, j))
+    return possible_actions
+
+
+def result(board, action):
+    """
+    Returns the board that results from making move (i, j) on the board.
+    """
+    i, j = action
+    if board[i][j] is not EMPTY:
+        raise Exception("Invalid move")
+    
+    new_board = copy.deepcopy(board)
+    new_board[i][j] = player(board)
+    return new_board
+
+
+def winner(board):
+    """
+    Returns the winner of the game, if there is one.
+    """
+    for row in board:
+        if row.count(X) == 3:
+            return X
+        elif row.count(O) == 3:
+            return O
+
+    for j in range(3):
+        column = [board[i][j] for i in range(3)]
+        if column.count(X) == 3:
+            return X
+        elif column.count(O) == 3:
+            return O
+
+    diagonal1 = [board[i][i] for i in range(3)]
+    diagonal2 = [board[i][2 - i] for i in range(3)]
+
+    if diagonal1.count(X) == 3 or diagonal2.count(X) == 3:
+        return X
+    elif diagonal1.count(O) == 3 or diagonal2.count(O) == 3:
+        return O
+
+    return None
+
+
+
+def terminal(board):
+    """
+    Returns True if game is over, False otherwise.
+    """
+    return winner(board) is not None or all(all(cell is not EMPTY for cell in row) for row in board)
+
+
+def utility(board):
+    """
+    Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
+    """
+    winner_player = winner(board)
+    if winner_player == X:
+        return 1
+    elif winner_player == O:
+        return -1
+    else:
+        return 0
+
+
+def minimax(board):
+    """
+    Returns the optimal action for the current player on the board.
+    """
+    if terminal(board):
+        return None
+
+    current_player = player(board)
+
+    if current_player == X:
+        _, action = max_value(board)
+    else:
+        _, action = min_value(board)
+
+    return action
+
+def max_value(board):
+    if terminal(board):
+        return utility(board), None
+
+    v = -math.inf
+    best_action = None
+
+    for action in actions(board):
+        min_val, _ = min_value(result(board, action))
+        if min_val > v:
+            v = min_val
+            best_action = action
+
+    return v, best_action
+
+def min_value(board):
+    if terminal(board):
+        return utility(board), None
+
+    v = math.inf
+    best_action = None
+
+    for action in actions(board):
+        max_val, _ = max_value(result(board, action))
+        if max_val < v:
+            v = max_val
+            best_action = action
+
+    return v, best_action
